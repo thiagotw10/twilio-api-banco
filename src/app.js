bb1665ee5ff9773f66065be3d35b8915aeba1e90
thiagotw10 <74217944+thiagotw10@users.noreply.github.com>
2022-04-05 16:46:04 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,11 +18,6 @@ class App {
         this.app.use(express.json());
         this.app.use(morgan('dev'));
         this.app.use((req, res, next) =>{
-
-            res.header("Access-Controll-Allow-Origin", "*");
-            res.header("Access-Controll-Allow-Methods", "Get, POST, PUT, DELETE");
-            res.header("Access-Controll-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-With")
-      
             this.app.use(cors());
             next();
         })
