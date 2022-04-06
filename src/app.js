e53bf35bf51e27d884a9f32bb766abe6c60fba75
thiagotw10 <74217944+thiagotw10@users.noreply.github.com>
2022-04-06 00:52:52 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,9 +20,6 @@ class App {
         this.app.use((req, res, next) =>{
             
             
-           res.header("Access-Controll-Allow-Origin", "https://thiagotw10.github.io/semantix-front/");
-           res.header("Access-Controll-Allow-Methods", "GET, POST, PUT, DELETE");
-           res.header("Access-Controll-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-With")
 
       
             this.app.use(cors({origin: "https://thiagotw10.github.io/semantix-front/", credentials: true}));
