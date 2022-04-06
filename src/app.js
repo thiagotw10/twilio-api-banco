7fc181e6012a06b2762ffb5439c45f04645e2db4
thiagotw10 <74217944+thiagotw10@users.noreply.github.com>
2022-04-06 01:16:49 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ class App {
         this.app.use((req, res, next) =>{
             
             
-           res.header("Access-Control-Allow-Origin", "https://thiagotw10.github.io");
+           res.header("Access-Control-Allow-Origin", "*");
            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
            res.header("Access-Control-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-With")
 
