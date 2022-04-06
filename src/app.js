0dbcf921f8f55ff6d474d7707940aadd64855ed9
thiagotw10 <74217944+thiagotw10@users.noreply.github.com>
2022-04-05 23:07:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ class App {
         this.app.use((req, res, next) =>{
             
             
-            res.header("Access-Controll-Allow-Origin", "https://semantix-api-2.herokuapp.com");
+            res.header("Access-Controll-Allow-Origin", "*");
             res.header("Access-Controll-Allow-Methods", "Get, POST, PUT, DELETE");
             res.header("Access-Controll-Allow-Headers", "Access, Content-type, Authorization, Acept, Origin, X-Requested-With")
       
